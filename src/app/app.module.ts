7595e1d3050f609b2dbe4b7f01fefdfc60fc176f
Carol Trindade <caroline.alvestrindade@hotmail.com>
2022-08-18 03:33:19 -0300

    topo do menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuNavComponent } from './menu-nav/menu-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuNavComponent
   ],
   imports: [
     BrowserModule,
